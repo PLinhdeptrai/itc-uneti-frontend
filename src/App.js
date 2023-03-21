4a8375173442e8dc6b6e62fcfe3ff667996d00d6
PLinhdeptrai <baclinhdz@gmail.com>
2023-03-21 21:34:27 +0700

    [UPDATE] cap nhat code

--- src/App.js
+++ src/App.js
@@ -2,25 +2,14 @@ import React from "react";
 import { Login, Register, ForgotPassword, Score, HomePage, Public } from "./client/screen/indexscreen";
 import { Routes, Route } from "react-router-dom";
 import path from "./utils/path/path";
-<<<<<<< HEAD
 import LoginApi from './client/screen/login/LoginApi';
-=======
 import CourItem from "./Layouts/LayoutCour/CourItem";
->>>>>>> master
 function App() {
 
   return (
 
     <div>
       <Routes>
-<<<<<<< HEAD
-        <Route path={path.PUBLIC} element={<Public />}>
-          <Route path={path.HOME} element={<HomePage />} />
-          <Route path={path.LOGIN} element={<Login />} />
-          <Route path={path.REGISTER} element={<Register />} />
-          <Route path={path.FORGOTPASSWORD} element={<ForgotPassword />} />
-          <Route path={path.SCORE} element={<Score />} />
-=======
         <Route path={path.PUBLIC} element={<Public/>}>
           <Route path={path.HOME} element={<HomePage/>}/>
           <Route path={path.LOGIN} element={<Login/>}/>
@@ -29,7 +18,6 @@ function App() {
           <Route path={path.SCORE} element={<Score/>}/>
           <Route path={path.COURSE} element={<CourItem />}/>
           {/* <Route path='./' element={<Score/>}/> */}
->>>>>>> master
         </Route>
       </Routes>
     </div>
--- src/client/screen/homePage/homePage.js
+++ src/client/screen/homePage/homePage.js
@@ -2,22 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import './homePage.css'
 import Header from "../../../components/header";
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
 import Slider from "../../../components/slider";
-=======
-import Slider from "../../../components/slider";
-<<<<<<< Updated upstream
-import News from "../../../components/News/News";
-import Cour from '../../../components/Cour/Cour'
-import Contest from '../../../components/Contest/Contest'
-import Doccument from "../../../components/Doccument/Doccument";
-import Footer from "../../../components/Footer/Footer";
-=======
->>>>>>> master
-import LoginApi from "../login/LoginApi";
->>>>>>> Stashed changes
 const HomePage = () => {
 
     // useEffect(() => {
@@ -36,15 +21,7 @@ const HomePage = () => {
     // }, [])
     return (
         <div>
-            <Header />
-            <div className="container-homepage">
-                <Slider />
-                <News />
-                <Cour />
-                <Contest />
-                <Doccument />
-            </div>
-            <Footer />
+            <Header></Header>
             <Outlet/>
         </div>
     )
